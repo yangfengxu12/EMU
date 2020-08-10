cb34c531d66cfa26af42583167c00bea0f8a247b
yangfengxu <yangfengxu96@126.com>
2020-08-10 18:03:39 +0800

    Add SX1276_Packet_detector code

---
+++ code/SX1276_Packet_detector/STM32CubeExpansion_LRWAN_V1.3.1/Projects/STM32L476RG-Nucleo/Applications/LoRa/PingPong/LoRaWAN/App/src/main.c
@@ -0,0 +1,318 @@
+/*!
+ * \file      main.c
+ *
+ * \brief     Ping-Pong implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *                ______                              _
+ *               / _____)             _              | |
+ *              ( (____  _____ ____ _| |_ _____  ____| |__
+ *               \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ *               _____) ) ____| | | || |_| ____( (___| | | |
+ *              (______/|_____)_|_|_| \__)_____)\____)_| |_|
+ *              (C)2013-2017 Semtech
+ *
+ * \endcode
+ *
+ * \author    Miguel Luis ( Semtech )
+ *
+ * \author    Gregory Cristian ( Semtech )
+ */
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @brief   this is the main!
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2018 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include "hw.h"
+#include "radio.h"
+#include "timeServer.h"
+#include "low_power_manager.h"
+#include "vcom.h"
+#include "sx1276.h"
+
+#define RF_FREQUENCY                                470000000-5000 // Hz
+
+
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#if defined( USE_MODEM_LORA )
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+//  1: 250 kHz,
+//  2: 500 kHz,
+//  3: Reserved]
+#define LORA_SPREADING_FACTOR                       11         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+//  2: 4/6,
+//  3: 4/7,
+//  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         5         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#elif defined( USE_MODEM_FSK )
+
+#define FSK_FDEV                                    25000     // Hz
+#define FSK_DATARATE                                50000     // bps
+#define FSK_BANDWIDTH                               50000     // Hz
+#define FSK_AFC_BANDWIDTH                           83333     // Hz
+#define FSK_PREAMBLE_LENGTH                         5         // Same for Tx and Rx
+#define FSK_FIX_LENGTH_PAYLOAD_ON                   false
+
+#else
+#error "Please define a modem in the compiler options."
+#endif
+
+typedef enum
+{
+  LOWPOWER,
+  RX,
+  RX_TIMEOUT,
+  RX_ERROR,
+  TX,
+  TX_TIMEOUT,
+} States_t;
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 64 // Define the payload size here
+#define LED_PERIOD_MS               200
+
+#define LEDS_OFF   do{ \
+                   LED_Off( LED_BLUE ) ;   \
+                   LED_Off( LED_RED ) ;    \
+                   LED_Off( LED_GREEN1 ) ; \
+                   LED_Off( LED_GREEN2 ) ; \
+                   } while(0) ;
+
+const uint8_t PingMsg[] = "PING";
+const uint8_t PongMsg[] = "PONG";
+
+uint16_t BufferSize = BUFFER_SIZE;
+uint8_t Buffer[BUFFER_SIZE];
+
+States_t State = LOWPOWER;
+
+int8_t RssiValue = 0;
+int8_t SnrValue = 0;
+
+/* Led Timers objects*/
+static  TimerEvent_t timerLed;
+
+/* Private function prototypes -----------------------------------------------*/
+/*!
+ * Radio events function pointer
+ */
+static RadioEvents_t RadioEvents;
+
+/*!
+ * \brief Function to be executed on Radio Tx Done event
+ */
+void OnTxDone(void);
+
+/*!
+ * \brief Function to be executed on Radio Rx Done event
+ */
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
+
+/*!
+ * \brief Function executed on Radio Tx Timeout event
+ */
+void OnTxTimeout(void);
+
+/*!
+ * \brief Function executed on Radio Rx Timeout event
+ */
+void OnRxTimeout(void);
+
+/*!
+ * \brief Function executed on Radio Rx Error event
+ */
+void OnRxError(void);
+
+/*!
+ * \brief Function executed on when led timer elapses
+ */
+static void OnledEvent(void *context);
+/**
+ * Main application entry point.
+ */
+int main(void)
+{
+  bool isMaster = true;
+  uint8_t i,reg;
+
+  HAL_Init();
+
+  SystemClock_Config();
+
+  DBG_Init();
+
+  HW_Init();
+
+  /*Disbale Stand-by mode*/
+  LPM_SetOffMode(LPM_APPLI_Id, LPM_Disable);
+
+  /* Led Timers*/
+  TimerInit(&timerLed, OnledEvent);
+  TimerSetValue(&timerLed, LED_PERIOD_MS);
+
+  TimerStart(&timerLed);
+
+  // Radio initialization
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.RxDone = OnRxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  RadioEvents.RxTimeout = OnRxTimeout;
+  RadioEvents.RxError = OnRxError;
+
+  Radio.Init(&RadioEvents);
+
+  Radio.SetChannel(RF_FREQUENCY);
+
+
+  Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    true, 0, 0, LORA_IQ_INVERSION_ON, 3000);
+
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+
+
+	printf("123\r\n");
+
+  Radio.Rx(RX_TIMEOUT_VALUE);
+	printf("FREQ:%d,sf:%d\r\n",RF_FREQUENCY,LORA_SPREADING_FACTOR);
+  while (1)
+  {
+    ENABLE_IRQ();
+		
+		reg=SX1276Read(0x18);
+		if((reg & 0x04) == 0x04)
+		{
+//			printf("Rx is going\r\n");
+
+			if((reg & 0x01) == 0x01)
+			{
+				printf("1.Signal detected\r\n");
+
+				if( (reg & 0x02) == 0x02 )
+				{
+					printf("2.Signal synchronized\r\n");
+
+					if( (reg & 0x08) == 0x08 )
+					{
+						printf("3.Header info validhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\r\n");
+
+					}
+					else
+					{
+						printf("0.Header info not valid\r\n");
+					}
+				}
+				else				
+				{
+					printf("0.Signal not synchronized\r\n");
+				}
+			}
+			else
+			{
+//				printf("Signal Not detected\r\n");
+			}
+		}
+		else
+		{
+//			printf("Rx isn't going\r\n");
+			Radio.Rx(RX_TIMEOUT_VALUE);
+		}
+		DelayMs(50);
+  }
+}
+
+void OnTxDone(void)
+{
+  Radio.Sleep();
+  State = TX;
+  PRINTF("OnTxDone\n\r");
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
+{
+  uint8_t i;
+	Radio.Sleep();
+  BufferSize = size;
+  memcpy(Buffer, payload, BufferSize);
+  RssiValue = rssi;
+  SnrValue = snr;
+  State = RX;
+	printf("\r\n");
+	for(i=0;i<BufferSize;i++)
+		printf("%x   ",Buffer[i]);
+	printf("\r\n");
+  printf("OnRxDone\n\r");
+  printf("RssiValue=%d dBm, SnrValue=%d\n\r", rssi, snr);
+	printf("\r\n");
+}
+
+void OnTxTimeout(void)
+{
+  Radio.Sleep();
+  State = TX_TIMEOUT;
+
+  PRINTF("OnTxTimeout\n\r");
+}
+
+void OnRxTimeout(void)
+{
+  Radio.Sleep();
+  State = RX_TIMEOUT;
+//  PRINTF("OnRxTimeout\n\r");
+}
+
+void OnRxError(void)
+{
+  Radio.Sleep();
+  State = RX_ERROR;
+  PRINTF("OnRxError\n\r");
+}
+
+static void OnledEvent(void *context)
+{
+  LED_Toggle(LED_BLUE) ;
+  LED_Toggle(LED_RED1) ;
+  LED_Toggle(LED_RED2) ;
+  LED_Toggle(LED_GREEN) ;
+
+  TimerStart(&timerLed);
+}
+
+
+int fputc(int ch, FILE *f)
+{ 	
+	while((USART2->ISR &0X40)==0);//Loop until the end of the transmit
+	USART2->TDR  = (uint8_t) ch;  
+	return ch;
+}
+
